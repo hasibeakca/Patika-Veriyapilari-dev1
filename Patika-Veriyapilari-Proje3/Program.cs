5857262631a431897b0277464f6a225f5bae3135
Hasibe Akca <hasibetosun42@gmail.com>
2024-01-22 17:57:18 +0300

    Son ödev

---
+++ Patika-Veriyapilari-Proje3/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Proje 3
+[7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız.
+
+Örnek: root x'dir. root'un sağından y bulunur. Solunda z bulunur vb.
+
+
+
+*********************************************************************************
+Binary Search tree oluştururken rootu 7 seçtim.
+Kural: Sol tarafında küçük sağ tarafınsa büyük sayı olmasıdır. 
+1. dal:                    7
+                    /           \
+2. dal:            5              8
+                /   \               \
+3. dal:       1      6               9
+             /  \
+4. dal:     0    3
+                /  \
+5. dal:       2      4
+***********************************************************************************
+*/
